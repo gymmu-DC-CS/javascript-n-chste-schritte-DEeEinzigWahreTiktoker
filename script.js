67d7f696058e00f7d3c2e70c1c7702662dfe277b
DanielHackingTheMatrix <e255570@edu.sbl.ch>
2023-11-21 10:25:19 +0100

    changes

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    if (currentElement != "e") {
+    if (currentElement != "e")
+    if (currentElement != "E") {
     result.push(currentElement)
   }
   }
