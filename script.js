413f994840cbf795f71477f6305c14e681408525
DanielHackingTheMatrix <e255570@edu.sbl.ch>
2023-11-28 10:25:08 +0100

    Aufgabe 2,3,4

--- script.js
+++ script.js
@@ -13,5 +13,41 @@ export function aufgabe01(args) {
   return result.join("")
 }
 
+// Ufgob 2
+
+export function aufgabe02(args) {
+  return args.toUpperCase()
+}
 
 
+// Ufgob 3
+
+export function aufgabe03(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E") {
+    result++
+    }
+  }
+  return result
+}
+
+
+// Ufgob 4
+
+export function aufgabe04(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === " " && input[i-1] != "-"){
+      result++
+    }
+    
+}
+  return result + 1
+}
